2fb38ef6ab0d8a3e841ba043dd07b79291f839e9
nithinreddy0418 <128250020+nithinreddy0418@users.noreply.github.com>
2023-07-18 01:18:27 +0530

    Add files via upload

---
+++ springboot-backend/src/test/java/net/javaguides/springboot/EmployeeRepositoryTests.java
@@ -0,0 +1,98 @@
+package net.javaguides.springboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.Rollback;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Optional;
+
+@DataJpaTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class EmployeeRepositoryTests {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Test
+    @Order(1)
+    @Rollback(value = false)
+    public void saveEmployeeTest(){
+
+        Employee employee = Employee.builder()
+                .firstName("Ramesh")
+                .lastName("Fadatare")
+                .email("ramesh@gmail.com")
+                .build();
+
+        employeeRepository.save(employee);
+
+        Assertions.assertThat(employee.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    @Order(2)
+    public void getEmployeeTest(){
+
+        Employee employee = employeeRepository.findById(1L).get();
+
+        Assertions.assertThat(employee.getId()).isEqualTo(1L);
+
+    }
+
+    @Test
+    @Order(3)
+    public void getListOfEmployeesTest(){
+
+        List<Employee> employees = employeeRepository.findAll();
+
+        Assertions.assertThat(employees.size()).isGreaterThan(0);
+
+    }
+
+    @Test
+    @Order(4)
+    @Rollback(value = false)
+    public void updateEmployeeTest(){
+
+        Employee employee = employeeRepository.findById(1L).get();
+
+        employee.setEmail("ram@gmail.com");
+
+        Employee employeeUpdated =  employeeRepository.save(employee);
+
+        Assertions.assertThat(employeeUpdated.getEmail()).isEqualTo("ram@gmail.com");
+
+    }
+
+    @Test
+    @Order(5)
+    @Rollback(value = false)
+    public void deleteEmployeeTest(){
+
+        Employee employee = employeeRepository.findById(1L).get();
+
+        employeeRepository.delete(employee);
+
+        //employeeRepository.deleteById(1L);
+
+        Employee employee1 = null;
+
+        Optional<Employee> optionalEmployee = employeeRepository.findByEmail("ram@gmail.com");
+
+        if(optionalEmployee.isPresent()){
+            employee1 = optionalEmployee.get();
+        }
+
+        Assertions.assertThat(employee1).isNull();
+    }
+
+}
